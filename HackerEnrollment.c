3ea7210e22debb9276e6cb24209e4f3e8fe2deee
YardenSharoniTechnion <sha.yarden@campus.technion.ac.il>
2023-04-30 21:52:10 +0300

    properly builds of these files

--- HackerEnrollment.c
+++ HackerEnrollment.c
@@ -2,6 +2,17 @@
 
 typedef struct {
     int id;
+    int totalCredits;
+    int gpa;
+    char* name;
+    char* surname;
+    char* city;
+    char* department;
+} Student;
+
+typedef struct {
+    int id;
+    int capacity;
     IsraeliQueue queue;
 } CourseQueue;
 
@@ -9,3 +20,7 @@ typedef struct {
     CourseQueue* courses;
     int courseCount;
 } EnrollmentSystem_t;
+
+EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers) {
+
+}
--- HackerEnrollment.h
+++ HackerEnrollment.h
@@ -1,7 +1,10 @@
 #ifndef PROVIDED_HACKERENROLLMENT_H
 #define PROVIDED_HACKERENROLLMENT_H
 
-typedef EnrollmentSystem_t* EnrollmentSystem;
+#include "IsraeliQueue.h"
+#include <stdio.h>
+
+typedef EnrollmentSystem_t * EnrollmentSystem;
 
 
 #endif
